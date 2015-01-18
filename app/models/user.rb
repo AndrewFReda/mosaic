0441936d6a66477b900e7c07fb157ec5caa6035c
andrewfreda <andrewfreda@gmail.com>
2015-01-18 17:52:21 -0500

    Add an ActiveRecord::AssociationList for each type of picture stored on a User (composition, base, mosaics)   * the Picture model has a field corresponding to each of the three types 	* the User's ID is used as a foreign key in matching of the three column

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_length_of :password, minimum: 1
 
-  has_many :uploaded_pictures, { class_name: 'Picture', dependent: :destroy }
+  has_many :composition_pictures, class_name: 'Picture', foreign_key: 'composition_id', dependent: :destroy
+  has_many :base_pictures, class_name: 'Picture', foreign_key: 'base_id', dependent: :destroy
+  has_many :mosaics, class_name: 'Picture', foreign_key: 'mosaic_id', dependent: :destroy
 
 end
