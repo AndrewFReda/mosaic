6243a1e06546a1e5567a9374e15ba52dc22e4204
andrewfreda <andrewfreda@gmail.com>
2015-01-18 17:43:04 -0500

    update routes to better follow RESTful route conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  get  'upload', to: 'mosaics#upload', as: 'upload'
+ 
+  get  'upload',      to: 'mosaics#upload'
+  post 'upload_comp', to: 'mosaics#upload_comp', as: 'upload_comp'
+  post 'upload_base', to: 'mosaics#upload_base', as: 'upload_base'
 
   get  'users/login', to: 'users#login',     as: 'login_user'
   post 'users/login', to: 'users#login_user'
