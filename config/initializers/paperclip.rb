0e17d1306821b8075fb42e8c35ad7994886bc7aa
andrewfreda <andrewfreda@gmail.com>
2015-01-31 16:09:54 -0500

    Comment paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,12 @@
+# Set up routes for sending Paperclip attachments to Amazon S3:
+
+# Use of Picture name in route
 Paperclip.interpolates('name') do |attachment, style|
   attachment.instance.name.parameterize
 end
 
+# Use of Picture type as a string in route.
+# Use 'picture_type' to invoke
 Paperclip.interpolates('picture_type') do |attachment, style|
   if not attachment.instance.composition_id.nil?
     "composition-pictures".parameterize
@@ -12,6 +17,7 @@ Paperclip.interpolates('picture_type') do |attachment, style|
   end
 end
 
+# Use of User's email in route.
 Paperclip.interpolates('user_email') do |attachment, style|
   id = attachment.instance.base_id ||
         attachment.instance.mosaic_id || 
