99544026cb6d48e3220473f08617904d6ee068d3
andrewfreda <andrewfreda@gmail.com>
2015-02-02 10:20:51 -0500

    provide better email and password validation  * Email is commented out right now however since it was rejecting too many right emails. Need to re-evaluate   * Password length is set to 1 for convenience

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_uniqueness_of :email
-  validates_length_of :password, minimum: 1
+  before_save { self.email = email.downcase }
+  validates :email, presence: true,
+                    # TODO: Email validation is saying everything fails. Figure out why.
+                    #format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+ 
+  validates :password, length: { minimum: 1 }
 
   has_many :composition_pictures, class_name: 'Picture', foreign_key: 'composition_id', dependent: :destroy
   has_many :base_pictures, class_name: 'Picture', foreign_key: 'base_id', dependent: :destroy
