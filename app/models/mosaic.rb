975fddcec1cb9705d1fca730c4c022e31a294a7b
andrewfreda <andrewfreda@gmail.com>
2015-04-02 14:19:13 -0400

    Set grid rows to be integers, not floats

--- app/models/mosaic.rb
+++ app/models/mosaic.rb
@@ -5,11 +5,11 @@ class Mosaic < ActiveRecord::Base
   def set_grid_from_image(base_img)
     # Determine ratio of mosaic image based on aspect of base image
     if base_img.columns > base_img.rows
-      self.grid_columns = 120 * (base_img.columns / base_img.rows.to_f)
-      self.grid_rows    = 120.0
+      self.grid_columns = 120 * (base_img.columns / base_img.rows)
+      self.grid_rows    = 120
     else
-      self.grid_columns = 120.0
-      self.grid_rows    = 120 * (base_img.rows / base_img.columns.to_f)
+      self.grid_columns = 120
+      self.grid_rows    = 120 * (base_img.rows / base_img.columns)
     end
   end
 
