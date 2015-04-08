3a260c17c51a09084c3505299d4ec8020d6a325e
andrewfreda <andrewfreda@gmail.com>
2015-04-08 19:54:28 -0400

    refactor to clean up code presentation

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   post   'users/change_password', to: 'users#change_password', as: 'change_password'
   delete 'users/delete_pictures', to: 'users#delete_pictures', as: 'delete_pictures'
-  get    'users/login', to: 'users#new_login', as: 'login_user'
-  post   'users/login', to: 'users#login'
-  delete 'users/logout', to: 'users#logout', as: 'logout_user'
-  post   'upload', to: 'users#upload_pictures', as: 'upload'
+  get    'users/login',  to: 'users#new_login',       as: 'login_user'
+  post   'users/login',  to: 'users#login'
+  delete 'users/logout', to: 'users#logout',          as: 'logout_user'
+  post   'upload',       to: 'users#upload_pictures', as: 'upload'
   
   delete 'mosaics', to: 'pictures#delete_mosaic'
   post   'mosaics', to: 'pictures#create_mosaic'
