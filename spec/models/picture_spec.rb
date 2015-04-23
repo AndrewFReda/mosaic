a130f452ecf6dd1140dceb6942e41226c8bc3b67
andrewfreda <andrewfreda@gmail.com>
2015-04-22 22:43:35 -0400

    test Picture.getContentType()

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,3 +1,12 @@
 RSpec.describe Picture do
 
+  describe '#getContentType' do
+    it 'returns correct content type' do
+      picture = FactoryGirl.build(:picture)
+      contentType = picture.getContentType()
+
+      expect(contentType).to eq('image/png')
+    end
+  end
+
 end
