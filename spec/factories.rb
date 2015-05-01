cb341129b48778e3ffd606319fbc109e8c83c6e7
andrewfreda <andrewfreda@gmail.com>
2015-05-01 14:41:56 -0400

    Modify user_with_pictures factory to: > create three pictures, one of each picture type > accept argument to change number of each type of picture created

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,27 +4,29 @@ FactoryGirl.define do
   end
 
   factory :picture do
-    histogram
+    association :histogram
     sequence(:name) { |n| "_testpicture#{n}.png" }
     url 'test_url'
     type 'composition'
   end
 
   factory :user do
-
     sequence(:email) { |n| "_testemail#{n}@testhost.com" }
     password 'somepassword'
     password_confirmation { password }
 
-    # user_with_pictures will create post data after the user has been created
+    # user_with_pictures will create picture data after the user has been created
     factory :user_with_pictures do
-      # See FactorGirl documentation: has_many
       transient do
-        picture_count 3
+        count_per_picture_type 1
       end
 
       after(:create) do |user, evaluator|
-        create_list(:picture, evaluator.picture_count, user: user)
+        evaluator.count_per_picture_type.times do
+          create(:picture, type: 'composition', user: user)
+          create(:picture, type: 'base', user: user)
+          create(:picture, type: 'mosaic', user: user)
+        end
       end
     end
   end
