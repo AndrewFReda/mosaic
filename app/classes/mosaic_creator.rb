2b369c914875b8536afa6349f249eb8558367efd
andrewfreda <andrewfreda@gmail.com>
2015-05-18 12:21:22 -0400

    > Restructure code > Ensure proper rounding to maintain image aspect ratio > Use convert instead of composite as it is much faster

--- app/classes/mosaic_creator.rb
+++ app/classes/mosaic_creator.rb
@@ -15,51 +15,53 @@ class MosaicCreator
     @image
   end
 
-  # Determine aspect ratio of this Mosaic based on its Base Picture
-  def set_aspect_ratio
-    @columns   = 25
-    @rows      = 25
-    base_image_columns = @base_image["%w"].to_i
-    base_image_rows    = @base_image["%h"].to_i
-    binding.pry
-    if base_image_columns > base_image_rows
-      @columns *= (base_image_columns / base_image_rows)
-    else
-      @rows    *= (base_image_rows / base_image_columns)
+  private
+    # Determine aspect ratio of this Mosaic based on its Base Picture
+    def set_aspect_ratio
+      @columns   = 25
+      @rows      = 25
+      base_image_columns = @base_image["%w"].to_i
+      base_image_rows    = @base_image["%h"].to_i
+
+      if base_image_columns > base_image_rows
+        @columns *= (base_image_columns / base_image_rows.to_f)
+        @columns = @columns.round
+      else
+        @rows *= (base_image_rows / base_image_columns.to_f)
+        @rows = @rows.round
+      end
+
+      temp_file = Tempfile.new(['render_composite_image', '.jpg'])
+      @image = MiniMagick::Image.new(temp_file.path, temp_file)
+      # Have to run this command to actually create the files
+      @image.run_command(:convert, "-size", "#{@columns * 60}x#{@rows * 60}", "xc:white", @image.path)
     end
 
-    temp_file = Tempfile.new(['render_composite_image', '.jpg'])
-    @image = MiniMagick::Image.new(temp_file.path, temp_file)
-    # Have to run this command to actually create the files
-    @image.run_command(:convert, "-size", "#{@columns * 60}x#{@rows * 60}", "xc:white", @image.path)
-  end
+    def create
+      # Divide base image into a grid that is basis mosaic grid
+      # Determine *EXACT* height/width of the individual base image cells 
+      base_cell_width  = @base_image["%w"].to_f / @columns
+      base_cell_height = @base_image["%h"].to_f / @rows
+      dimensions = "#{base_cell_width}x#{base_cell_height}"
+      convert = MiniMagick::Tool::Convert.new
 
-  def create
-    # Divide base image into a grid that is basis mosaic grid
-    # Determine *EXACT* height/width of the individual base image cells 
-    base_cell_width  = @base_image["%w"].to_i / @columns.to_f
-    base_cell_height = @base_image["%h"].to_i / @rows.to_f
-    binding.pry
-
-    # iterate through the grid that represents the final mosaic
-    @columns.times do |c|
-      @rows.times do |r|
-        image      = MiniMagick::Image.open(@base_image.path)
-        dimensions = "#{base_cell_width}x#{base_cell_height}"
-        # Need to round else it will always round down
-        offsets    = "+#{(base_cell_width * c).round}+#{(base_cell_height * r).round}"
-
-        # MiniMagick#crop
-        image.crop "#{dimensions}#{offsets}" # +repage"
-        # Find image from cache with hue matching given image
-        image = @cache.find_matching_image image
-
-        @image = @image.composite(image) do |comp|
-          comp.gravity "NorthWest"
-          # TODO: Do scaling better  
-          comp.geometry "+#{c * 60}+#{r * 60}"
+      # iterate through the grid that represents the final mosaic
+      @columns.times do |c|
+        @rows.times do |r|
+          image   = MiniMagick::Image.open(@base_image.path)
+          # MiniMagick#crop
+          image.crop "#{dimensions}+#{(base_cell_width * c)}+#{(base_cell_height * r)}" # +repage"
+          # Find image from cache with hue matching given image
+          matching = @cache.find_matching_image image
+          convert << '-page' << "+#{(60 * c)}+#{(60 * r)}" << matching.path
+          #image.destroy!
+          puts "#{'*' * c} #{'>' * r}"
         end
       end
+      binding.pry
+      convert << '-mosaic' << 'tmp/mosaic.png'
+      convert.call
+      binding.pry
+      @image 
     end
-  end
 end
