6055e4dcf7dab3e4c755a6bf5ec47c31d98bf8d1
andrewfreda <andrewfreda@gmail.com>
2015-05-18 12:50:19 -0400

    Update mosaic creation methods to use append instead of -layers mosaic > Based on benchmarking script just added the append method is faster even with writing and reading from tmp

--- app/classes/mosaic_creator.rb
+++ app/classes/mosaic_creator.rb
@@ -30,11 +30,6 @@ class MosaicCreator
         @rows *= (base_image_rows / base_image_columns.to_f)
         @rows = @rows.round
       end
-
-      temp_file = Tempfile.new(['render_composite_image', '.jpg'])
-      @image = MiniMagick::Image.new(temp_file.path, temp_file)
-      # Have to run this command to actually create the files
-      @image.run_command(:convert, "-size", "#{@columns * 60}x#{@rows * 60}", "xc:white", @image.path)
     end
 
     def create
@@ -43,25 +38,31 @@ class MosaicCreator
       base_cell_width  = @base_image["%w"].to_f / @columns
       base_cell_height = @base_image["%h"].to_f / @rows
       dimensions = "#{base_cell_width}x#{base_cell_height}"
-      convert = MiniMagick::Tool::Convert.new
+      mosaic = MiniMagick::Tool::Convert.new
 
       # iterate through the grid that represents the final mosaic
-      @columns.times do |c|
-        @rows.times do |r|
-          image   = MiniMagick::Image.open(@base_image.path)
+      @rows.times do |r|
+        row = MiniMagick::Tool::Convert.new
+
+        @columns.times do |c|
+          image = MiniMagick::Image.open(@base_image.path)
           # MiniMagick#crop
-          image.crop "#{dimensions}+#{(base_cell_width * c)}+#{(base_cell_height * r)}" # +repage"
+          image.crop "#{dimensions}+#{(base_cell_width * c)}+#{(base_cell_height * r)}"
           # Find image from cache with hue matching given image
           matching = @cache.find_matching_image image
-          convert << '-page' << "+#{(60 * c)}+#{(60 * r)}" << matching.path
+          row << matching.path
           #image.destroy!
-          puts "#{'*' * c} #{'>' * r}"
         end
+
+        row_path = "tmp/row-#{r}.png"
+        row << '+append' << row_path
+        row.call
+
+        mosaic << row_path
       end
-      binding.pry
-      convert << '-mosaic' << 'tmp/mosaic.png'
-      convert.call
-      binding.pry
-      @image 
+
+      mosaic << '-append' << 'tmp/mosaic.png'
+      mosaic.call
+      @image = MiniMagick::Image.new('tmp/mosaic.png')
     end
 end
